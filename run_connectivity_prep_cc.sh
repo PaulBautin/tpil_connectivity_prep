cda02dea83d56b698d1fb05d8135e50b76de4da4
PaulBautin <pbautin70@gmail.com>
2023-08-16 17:32:59 -0400

    add syn.sh

--- run_connectivity_prep_cc.sh
+++ run_connectivity_prep_cc.sh
@@ -14,7 +14,7 @@
                                # https://docs.computecanada.ca/wiki/B%C3%A9luga/en#Node_Characteristics
 #SBATCH --mem=0                # --> 0 means you take all the memory of the node. If you think you will need
                                # all the node, you can keep 0.
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 
 #SBATCH --mail-user=paul.bautin@polymtl.ca
 #SBATCH --mail-type=BEGIN
@@ -29,8 +29,8 @@ module load StdEnv/2020 java/14.0.2 nextflow/22.10.8 apptainer/1.1.8
 
 my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/singularity_container.sif' # or .sif
 my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_connectivity_prep/main.nf'
-my_input_tr='/home/pabaua/scratch/tpil_dev/results/clbp/23-07-05_tractoflow_bundling/results'
-my_input_fs='/home/pabaua/scratch/tpil_dev/data/22-09-21_t1_clbp_freesurfer_output'
+my_input_tr='/home/pabaua/scratch/tpil_dev/results/control/23-07-05_tractoflow_bundling/results'
+my_input_fs='/home/pabaua/scratch/tpil_dev/data/23_02_09_control_freesurfer_output'
 my_licence_fs='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_connectivity_prep/freesurfer_data/license.txt'
 
 
